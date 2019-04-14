b843a12e06468892208bcce40b871a63c6e79297
zqrtalent <zaqroo@gmail.com>
2019-04-14 15:01:58 +0200

    Comment heroku release command

--- scripts/deploy-heroku-docker.sh
+++ scripts/deploy-heroku-docker.sh
@@ -6,7 +6,11 @@ USER_NAME=$1 #Heroku username
 HEROKU_APP=$2 #Heroku app name
 HEROKU_APP_PROCESS_TYPE="web"
 HEROKU_API_KEY=$3 #Heroku api key
-BUILD_DIR=$4
+BUILD_DIR='..'
+
+if [ $4 != '' ]; then
+  BUILD_DIR=$4
+fi
 
 #Login in docker registry.
 docker login --username=$USER_NAME --password=$HEROKU_API_KEY $DOCKER_REGISTRY_URL
@@ -19,17 +23,17 @@ docker tag ${HEROKU_APP}_image ${DOCKER_REGISTRY_URL}/${HEROKU_APP}/${HEROKU_APP
 docker push ${DOCKER_REGISTRY_URL}/${HEROKU_APP}/${HEROKU_APP_PROCESS_TYPE}
 
 #Get docker image id.
-DOCKER_IMAGE_ID=$(docker inspect ${HEROKU_APP}_image --format={{.Id}})
+#DOCKER_IMAGE_ID=$(docker inspect ${HEROKU_APP}_image --format={{.Id}})
 
 #Release heroku app
-curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation \
-  -d '{
-  "updates": [
-    {
-      "type": "'"$HEROKU_APP_PROCESS_TYPE"'",
-      "docker_image": "'"$DOCKER_IMAGE_ID"'"
-    }
-  ]
-}' \
-  -H "Content-Type: application/json" \
-  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
+#curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation \
+#  -d '{
+#  "updates": [
+#    {
+#      "type": "'"$HEROKU_APP_PROCESS_TYPE"'",
+#      "docker_image": "'"$DOCKER_IMAGE_ID"'"
+#    }
+#  ]
+#}' \
+#  -H "Content-Type: application/json" \
+#  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
