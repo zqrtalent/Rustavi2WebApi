7e2f95f31dd2b94a896c9c4fc819d7876c0bc26c
zqrtalent <zaqroo@gmail.com>
2019-04-14 19:15:52 +0200

    Heroku release command update

--- scripts/deploy-heroku-docker.sh
+++ scripts/deploy-heroku-docker.sh
@@ -6,9 +6,10 @@ USER_NAME=$1 #Heroku username
 HEROKU_APP=$2 #Heroku app name
 HEROKU_APP_PROCESS_TYPE="web"
 HEROKU_API_KEY=$3 #Heroku api key
-BUILD_DIR='..'
+BUILD_DIR='../Rustavi2WebApi'
+BASIC_AUTH_VAL=$(echo ':${HEROKU_API_KEY}' | base64)
 
-if [ $4 != '' ]; then
+if [ "$4" != "" ]; then
   BUILD_DIR=$4
 fi
 
@@ -23,17 +24,18 @@ docker tag ${HEROKU_APP}_image ${DOCKER_REGISTRY_URL}/${HEROKU_APP}/${HEROKU_APP
 docker push ${DOCKER_REGISTRY_URL}/${HEROKU_APP}/${HEROKU_APP_PROCESS_TYPE}
 
 #Get docker image id.
-#DOCKER_IMAGE_ID=$(docker inspect ${HEROKU_APP}_image --format={{.Id}})
+DOCKER_IMAGE_ID=$(docker inspect ${HEROKU_APP}_image --format={{.Id}})
 
 #Release heroku app
-#curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation \
-#  -d '{
-#  "updates": [
-#    {
-#      "type": "'"$HEROKU_APP_PROCESS_TYPE"'",
-#      "docker_image": "'"$DOCKER_IMAGE_ID"'"
-#    }
-#  ]
-#}' \
-#  -H "Content-Type: application/json" \
-#  -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
+curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation \
+  -d '{
+  "updates": [
+    {
+      "type": "'"$HEROKU_APP_PROCESS_TYPE"'",
+      "docker_image": "'"$DOCKER_IMAGE_ID"'"
+    }
+  ]
+}' \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer ${HEROKU_API_KEY}" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
